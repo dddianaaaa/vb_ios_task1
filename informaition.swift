e62e03cfe2c0e96a3e0ae5f13c238923d7abb729
dddianaaaa <93527637+dddianaaaa@users.noreply.github.com>
2024-03-25 20:30:35 +0300

    Create informaition.swift

---
+++ informaition.swift
@@ -0,0 +1,7 @@
+import Foundation
+let name :String = "Diana"
+var surname = "Dzeyn"
+var mood="very happy"
+var age : UInt32 = 21
+print("Hello everyone! My name is \(name),my surname is \(surname),i'm \(age) years old.",
+"\n and i'm \(mood)")
